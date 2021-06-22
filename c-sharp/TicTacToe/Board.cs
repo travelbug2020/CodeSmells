e210e1e9bd701408d63332e8607dfce9f72ef75f
Rachel Whitmore <rachel.whitmore@mandg.com>
2021-06-22 13:53:46 +0100

    r

---
+++ c-sharp/TicTacToe/Board.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TicTacToe
+{
+    // SHOT GUN SURGERY
+    // LARGE CLASS - CODE SMELL
+    public class Board
+    {
+        private List<Tile> _plays = new List<Tile>();
+
+        public Board()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    _plays.Add(new Tile { X = i, Y = j, Symbol = ' ' });
+                }
+            }
+        }
+        //PRIMITIVE OBBSESSION
+        //DATA CLUMP
+        public Tile TileAt(int x, int y)
+        {
+            return _plays.Single(tile => tile.X == x && tile.Y == y);
+        }
+        //LONG PARAMETER LIST
+        //PRIMITIVE OBBSESSION
+        //DATA CLUMPS
+        public void AddTileAt(char symbol, int x, int y)
+        {
+            //Message Chain
+            _plays.Single(tile => tile.X == x && tile.Y == y).Symbol = symbol;
+        }
+    }
+}
--- c-sharp/TicTacToe/Game.cs
+++ c-sharp/TicTacToe/Game.cs
@@ -1,127 +1,115 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-
+using System.Security.Cryptography.X509Certificates;
+//board tile game
 namespace TicTacToe
 {
-    public class Tile
-    {
-        public int X {get; set;}
-        public int Y {get; set;}
-        public char Symbol {get; set;}
-    }
-
-    public class Board
-    {
-       private List<Tile> _plays = new List<Tile>();
-       
-        public Board()
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    _plays.Add(new Tile{ X = i, Y = j, Symbol = ' '});
-                }  
-            }       
-        }
-       public Tile TileAt(int x, int y)
-       {
-           return _plays.Single(tile => tile.X == x && tile.Y == y);
-       }
-
-       public void AddTileAt(char symbol, int x, int y)
-       {
-           var newTile = new Tile
-           {
-               X = x,
-               Y = y,
-               Symbol = symbol
-           };
-
-           _plays.Single(tile => tile.X == x && tile.Y == y).Symbol = symbol;
-       }
-    }
-
+    // DIVERGANT CHANGE
+    // COMMENTS
     public class Game
     {
+        //PRIMITIVE OBBSESSION
         private char _lastSymbol = ' ';
         private Board _board = new Board();
-        
+
+        //LONG PARAMETER LIST
+        //PRIMITIVE OBBSESSION
+        //DATA CLUMP
         public void Play(char symbol, int x, int y)
         {
-            //if first move
-            if(_lastSymbol == ' ')
-            {
-                //if player is X
-                if(symbol == 'O')
-                {
-                    throw new Exception("Invalid first player");
-                }
-            } 
-            //if not first move but player repeated
-            else if (symbol == _lastSymbol)
-            {
-                throw new Exception("Invalid next player");
-            }
-            //if not first move but play on an already played tile
-            else if (_board.TileAt(x, y).Symbol != ' ')
-            {
-                throw new Exception("Invalid position");
-            }
-
-            // update game state
+            EnsureValidMove(symbol, x, y);
             _lastSymbol = symbol;
             _board.AddTileAt(symbol, x, y);
         }
 
-        public char Winner()
-        {   //if the positions in first row are taken
-            if(_board.TileAt(0, 0).Symbol != ' ' &&
-               _board.TileAt(0, 1).Symbol != ' ' &&
-               _board.TileAt(0, 2).Symbol != ' ')
-               {
-                    //if first row is full with same symbol
-                    if (_board.TileAt(0, 0).Symbol == 
-                        _board.TileAt(0, 1).Symbol &&
-                        _board.TileAt(0, 2).Symbol == 
-                        _board.TileAt(0, 1).Symbol )
-                        {
-                            return _board.TileAt(0, 0).Symbol;
-                        }
-               }
-                
-             //if the positions in first row are taken
-             if(_board.TileAt(1, 0).Symbol != ' ' &&
-                _board.TileAt(1, 1).Symbol != ' ' &&
-                _board.TileAt(1, 2).Symbol != ' ')
-                {
-                    //if middle row is full with same symbol
-                    if (_board.TileAt(1, 0).Symbol == 
-                        _board.TileAt(1, 1).Symbol &&
-                        _board.TileAt(1, 2).Symbol == 
-                        _board.TileAt(1, 1).Symbol)
-                        {
-                            return _board.TileAt(1, 0).Symbol;
-                        }
-                }
+        private void EnsureValidMove(char symbol, int x, int y)
+        {
+            if (IsInvalidFirstPlayer(symbol))
+            {
+                throw new Exception("Invalid first player");
+            }
 
-            //if the positions in first row are taken
-             if(_board.TileAt(2, 0).Symbol != ' ' &&
-                _board.TileAt(2, 1).Symbol != ' ' &&
-                _board.TileAt(2, 2).Symbol != ' ')
-                {
-                    //if middle row is full with same symbol
-                    if (_board.TileAt(2, 0).Symbol == 
-                        _board.TileAt(2, 1).Symbol &&
-                        _board.TileAt(2, 2).Symbol == 
-                        _board.TileAt(2, 1).Symbol)
-                        {
-                            return _board.TileAt(2, 0).Symbol;
-                        }
-                }
+            if (IsPlayerRepeated(symbol))
+            {
+                throw new Exception("Invalid next player");
+            }
+
+            if (IsPositionAlreadyPlayed(x, y))
+            {
+                throw new Exception("Invalid position");
+            }
+        }
+
+        private bool IsInvalidFirstPlayer(char symbol)
+        {
+            return IsFirstMove() && IsPlayerO(symbol);
+        }
+
+        private bool IsPositionAlreadyPlayed(int x, int y)
+        {
+            //Message Chain
+            return _board.TileAt(x, y).Symbol != ' ';
+        }
+
+        private bool IsPlayerRepeated(char symbol)
+        {
+            return symbol == _lastSymbol;
+        }
+
+        private static bool IsPlayerO(char symbol)
+        {
+            return symbol == 'O';
+        }
+
+        private bool IsFirstMove()
+        {
+            return _lastSymbol == ' ';
+        }
+
+        //Primitive OBBSESSION - CODE SMELL
+        // FEATURE ENVY
+        //Innappropiate intimicy
+        //Message Chains
+        public char Winner()
+        {
+            if (IsWinningRow(0))
+            {
+                return GetSymbolAtPosition(0, 0);
+            }
+            if (IsWinningRow(1))
+            {
+                return GetSymbolAtPosition(1, 0);
+            }
+            if (IsWinningRow(2))
+            {
+                return GetSymbolAtPosition(2, 0);
+            }
 
             return ' ';
+        }
+
+        private bool IsWinningRow(int x)
+        {
+            return IsRowTaken(x) && HasRowGotSameSymbol(x);
+        }
+
+        private char GetSymbolAtPosition(int x, int y)
+        {
+            return _board.TileAt(x, y).Symbol;
+        }
+
+        private bool HasRowGotSameSymbol(int x)
+        {
+            return _board.TileAt(x, 0).Symbol ==
+                   _board.TileAt(x, 1).Symbol &&
+                   _board.TileAt(x, 2).Symbol ==
+                   _board.TileAt(x, 1).Symbol;
+        }
+
+        private bool IsRowTaken(int x)
+        {
+            return _board.TileAt(x, 0).Symbol != ' ' &&
+                   _board.TileAt(x, 1).Symbol != ' ' &&
+                   _board.TileAt(x, 2).Symbol != ' ';
         }
     }
 }
